a60188e616b08b16e375ba2cc87b4ba9f5ab2e5c
Matthew Lutrick <MattGL@live.com>
2025-10-24 13:29:42 -0500

    let us render some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Header from './components/Header';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
     </div>
   );
 }
